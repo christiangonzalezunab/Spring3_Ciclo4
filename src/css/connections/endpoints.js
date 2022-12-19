270e7d6669601014eb70d4489c5c1c9d8488531a
christiangonzalezunab <christian.gonzalez1@o365.unab.edu.co>
2022-12-18 22:37:15 -0500

    Spring3_Nivelación

---
+++ src/css/connections/endpoints.js
@@ -0,0 +1,4 @@
+const API="https://10d3bb19-7b05-41ae-9628-31d34c286cc5.mock.pstmn.io"
+
+export const PRODUCTOSCREADOS_GET_ENDPOINT = API + "/Productos"
+export const CREARPRODUCTO_POST_ENDPOINT = API + "/Productos"
